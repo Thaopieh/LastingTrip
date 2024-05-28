1f76743536636bdd9aa70bf0e83af60c3bbeda38
thaitrinh12100 <thaitrinh12100@gmail.com>
2024-05-28 14:20:28 +0700

    update

--- public/js/mainPage.js
+++ public/js/mainPage.js
@@ -61,12 +61,35 @@ $(document).ready(function () {
     },
   });
 
+  function ChangeToSlug(title) {
+    var slug;
+    slug = title.toLowerCase();
+    slug = slug.replace(/á|à|ả|ạ|ã|ă|ắ|ằ|ẳ|ẵ|ặ|â|ấ|ầ|ẩ|ẫ|ậ/gi, 'a');
+    slug = slug.replace(/é|è|ẻ|ẽ|ẹ|ê|ế|ề|ể|ễ|ệ/gi, 'e');
+    slug = slug.replace(/i|í|ì|ỉ|ĩ|ị/gi, 'i');
+    slug = slug.replace(/ó|ò|ỏ|õ|ọ|ô|ố|ồ|ổ|ỗ|ộ|ơ|ớ|ờ|ở|ỡ|ợ/gi, 'o');
+    slug = slug.replace(/ú|ù|ủ|ũ|ụ|ư|ứ|ừ|ử|ữ|ự/gi, 'u');
+    slug = slug.replace(/ý|ỳ|ỷ|ỹ|ỵ/gi, 'y');
+    slug = slug.replace(/đ/gi, 'd');
+    slug = slug.replace(/\`|\~|\!|\@|\#|\||\$|\%|\^|\&|\*|\(|\)|\+|\=|\,|\.|\/|\?|\>|\<|\'|\"|\:|\;|_/gi, '');
+    slug = slug.replace(/ /gi, "-");
+    slug = slug.replace(/\-\-\-\-\-/gi, '-');
+    slug = slug.replace(/\-\-\-\-/gi, '-');
+    slug = slug.replace(/\-\-\-/gi, '-');
+    slug = slug.replace(/\-\-/gi, '-');
+    slug = '@' + slug + '@';
+    slug = slug.replace(/\@\-|\-\@|\@/gi, '');
+    slug = slug.trim();
+    return slug;
+  }
+
   document.querySelectorAll(".hotelName").forEach(function (link) {
     link.addEventListener("click", function (event) {
       // Ngăn chặn hành động mặc định của thẻ <a>
       event.preventDefault();
       // Lấy tên của khách sạn từ nội dung của thẻ <a> được click
       var hotelName = this.textContent.trim();
+      var slug = ChangeToSlug(hotelName);
       // Gửi yêu cầu AJAX để tìm hotelId tương ứng
       $.ajax({
         url: "http://localhost:3030/api/v1/hotels/getIdByHotelName",
@@ -75,8 +98,9 @@ $(document).ready(function () {
         success: function (response) {
           // Xử lý dữ liệu phản hồi (response) từ máy chủ
           var hotelId = response.hotelId;
+
           console.log("Hotel ID:", hotelId);
-          window.location.href = "http://localhost:3030/hotel/" + hotelId;
+          window.location.href = "http://localhost:3030/hotel/" + slug + "/" + hotelId;
         },
         error: function (xhr, status, error) {
           console.error("Error:", error);
